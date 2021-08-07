e4440681125cb6de9c8038b63f05508ebbf3709a
mjbiener <michaelbiener@yahoo.com>
2021-08-07 12:38:27 -0400

    works - need to handle input errors/exceptions

--- mystery_word.py
+++ mystery_word.py
@@ -29,8 +29,9 @@ def start_game():
 def word_selection():
     global random_word
     random_word = []
+    
     difficulty = input("\nPlease enter a difficulty level by letter: easy = e, normal = n. hard = h: ").lower()
-    # program in hard stop if input more than one letter or is not one of listed letters!
+    # program in error/exception if input more than one letter or is not one of listed letters!
     # if difficulty != ("e" or "n" or "h"):
     #     print("please follow directions and enter a correct letter! ")
     #     input("\nPlease enter a difficulty level by letter: easy = e, normal = n. hard = h: ")
@@ -53,15 +54,23 @@ def word_selection():
     play_game()
 
 
-
 def play_game():
     print(random_word)
     max_guesses = 8
     guess_count = 1
+    global letters_guessed
     letters_guessed = []
+    progress_display = make_word(random_word, letters_guessed)
+    print(progress_display)
     user_input = input("Guess a letter: ").lower()
+   #need to finish handling exceptions/errors of input
+    if len(user_input) > 1:
+        print("Please follow the directions - Enter only one letter at a time")
+        user_input = input("Guess a letter: ").lower()
+        # need to finish handling exceptions/errors of input
+
     while user_input not in quit_words:
-        
+
         if guess_count == 8:
             print("\nYou have run out of guesses! Better luck next time.")
             print(f"The mystery word was {random_word}")
@@ -72,30 +81,52 @@ def play_game():
                 elif user_input not in start_words:
                     print("Goodbye!")
                     sys.exit()
-        
         elif user_input in letters_guessed:
             print("You already guessed that letter!")
+            print(f"you have guessed the following letters already {letters_guessed}")
+            progress_display = make_word(random_word, letters_guessed)
+            print(progress_display)
             user_input = input("Enter a different letter. ").lower()
-        
+            # need to finish handling exceptions/errors of input
         elif user_input not in letters_guessed and user_input in random_word:
             letters_guessed.append(user_input)
-
-            # add user_input to word tracking display
-
-            print(f"you have {max_guesses-guess_count + 1} guesses remaining")
-            user_input = input("Enter another letter. ").lower()
-        
+            print(f"you have guessed the following letters already {letters_guessed}")
+            progress_display = make_word(random_word, letters_guessed)
+            print(progress_display)
+            if "_" not in progress_display:
+                print("You Win! You are a mystery word master!")
+                user_input = input("Would you like to play again? Yes or No? ")
+                while(True):
+                    if user_input in start_words:
+                        word_selection()
+                    elif user_input not in start_words:
+                        print("Goodbye!")
+                        sys.exit()
+            else:
+                print(f"you have {max_guesses-guess_count + 1} guesses remaining")
+                user_input = input("\nEnter another letter. ").lower()
+                # need to finish handling exceptions/errors of input
         elif user_input not in (letters_guessed and random_word):
-            guess_count +=1
+            guess_count += 1
             letters_guessed.append(user_input)
-            print(f"you have {max_guesses-guess_count + 1} guesses remaining")
+            print(f"That letter is not in the word. You have {max_guesses-guess_count + 1} guesses remaining")
+            print(f"you have guessed the following letters already {letters_guessed}")
+            progress_display = make_word(random_word, letters_guessed)
+            print(progress_display)
             user_input = input("Enter a different letter. ").lower()
     print(letters_guessed)
 
 
-def word_tracking():
-    # code to display the random word as dashes then add letters as they are guessed
-    pass
+def show_word(letter, guesses):
+    if letter in guesses:
+        return letter
+    else:
+        return "_"
+
+
+def make_word(word, guesses):
+    output_letters = [show_word(letter, letters_guessed) for letter in random_word]
+    return(' '.join(output_letters))
 
 
 start_game()
