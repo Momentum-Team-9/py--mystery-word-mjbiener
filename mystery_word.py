2401b3254d492efbabcdc5d3b09e43dc6f356884
mjbiener <michaelbiener@yahoo.com>
2021-08-08 18:06:42 -0400

    fin

--- mystery_word.py
+++ mystery_word.py
@@ -1,5 +1,6 @@
 import random
 import sys
+import string
 
 
 with open("words.txt") as words_list:
@@ -29,29 +30,37 @@ def start_game():
 def word_selection():
     global random_word
     random_word = []
-    
+    difficulty_levels = ["e", "n", "h"]
     difficulty = input("\nPlease enter a difficulty level by letter: easy = e, normal = n. hard = h: ").lower()
-    # program in error/exception if input more than one letter or is not one of listed letters!
-    # if difficulty != ("e" or "n" or "h"):
-    #     print("please follow directions and enter a correct letter! ")
-    #     input("\nPlease enter a difficulty level by letter: easy = e, normal = n. hard = h: ")
+    print(type(difficulty_levels))
+    print(type(difficulty))
+
+    if difficulty not in difficulty_levels:
+        print("please follow directions and enter a correct letter to choose game level! ")
+
     if difficulty == "e":
         for word in words:
             if 4 <= len(word) <= 6:
                 easy_words.append(word)
                 random_word = random.choice(easy_words)
+                print(f"the mystery word has {len(random_word)} letters ")
+                play_game()
+                
     elif difficulty == "n":
         for word in words:
             if 6 <= len(word) <= 8:
                 normal_words.append(word)
                 random_word = random.choice(normal_words)
+                print(f"the mystery word has {len(random_word)} letters ")
+                play_game()
+                
     elif difficulty == "h":
         for word in words:
             if len(word) >= 8:
                 hard_words.append(word)
                 random_word = random.choice(hard_words)
-    print(f"the mystery word has {len(random_word)} letters ")
-    play_game()
+                print(f"the mystery word has {len(random_word)} letters ")
+                play_game()
 
 
 def play_game():
@@ -60,42 +69,27 @@ def play_game():
     guess_count = 1
     global letters_guessed
     letters_guessed = []
+    string.ascii_letters
+    valid_guesses = list(string.ascii_letters)
     progress_display = make_word(random_word, letters_guessed)
     print(progress_display)
     user_input = input("Guess a letter: ").lower()
-   #need to finish handling exceptions/errors of input
-    if len(user_input) > 1:
-        print("Please follow the directions - Enter only one letter at a time")
-        user_input = input("Guess a letter: ").lower()
-        # need to finish handling exceptions/errors of input
 
     while user_input not in quit_words:
 
-        if guess_count == 8:
-            print("\nYou have run out of guesses! Better luck next time.")
-            print(f"The mystery word was {random_word}")
-            user_input = input("Would you like to play again? Yes or No? ")
-            while(True):
-                if user_input in start_words:
-                    word_selection()
-                elif user_input not in start_words:
-                    print("Goodbye!")
-                    sys.exit()
-        elif user_input in letters_guessed:
-            print("You already guessed that letter!")
-            print(f"you have guessed the following letters already {letters_guessed}")
-            progress_display = make_word(random_word, letters_guessed)
-            print(progress_display)
-            user_input = input("Enter a different letter. ").lower()
-            # need to finish handling exceptions/errors of input
+        if len(user_input) > 1 or user_input not in valid_guesses:
+            print("Please follow the directions - Enter only one letter at a time")
+            user_input = input("Guess a letter: ").lower()
+
         elif user_input not in letters_guessed and user_input in random_word:
             letters_guessed.append(user_input)
+            print(f"\nGreat choice! {user_input} is in the word!")
             print(f"you have guessed the following letters already {letters_guessed}")
             progress_display = make_word(random_word, letters_guessed)
             print(progress_display)
             if "_" not in progress_display:
                 print("You Win! You are a mystery word master!")
-                user_input = input("Would you like to play again? Yes or No? ")
+                user_input = input("\nWould you like to play again? Yes or No? ")
                 while(True):
                     if user_input in start_words:
                         word_selection()
@@ -105,7 +99,25 @@ def play_game():
             else:
                 print(f"you have {max_guesses-guess_count + 1} guesses remaining")
                 user_input = input("\nEnter another letter. ").lower()
-                # need to finish handling exceptions/errors of input
+
+        elif guess_count == max_guesses:
+            print("\nYou have run out of guesses! Better luck next time.")
+            print(f"The mystery word was {random_word}")
+            user_input = input("Would you like to play again? Yes or No? ")
+            while(True):
+                if user_input in start_words:
+                    word_selection()
+                elif user_input not in start_words:
+                    print("Goodbye!")
+                    sys.exit()
+
+        elif user_input in letters_guessed:
+            print("You already guessed that letter!")
+            print(f"you have guessed the following letters already {letters_guessed}")
+            progress_display = make_word(random_word, letters_guessed)
+            print(progress_display)
+            user_input = input("Enter a different letter. ").lower()
+
         elif user_input not in (letters_guessed and random_word):
             guess_count += 1
             letters_guessed.append(user_input)
